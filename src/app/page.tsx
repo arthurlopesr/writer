22ec1afea58ecdc222228188ba25c264ba78f86c
Arthur Lopes <arthurlopr12@gmail.com>
2023-10-27 21:10:53 -0300

    feat: create poposition section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import { MaxWidthWrapper } from "./components/maxWidthWrapper/MaxWidthWrapper";
 import { HeroSection } from "./pages/home/heroSection/HeroSection";
+import { PropositionsSection } from "./pages/home/propositionSection/PropositionSection";
 
 export default function Home() {
   return (
-    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
-      <HeroSection />
-    </MaxWidthWrapper>
+    <>
+      <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+        <HeroSection />
+      </MaxWidthWrapper>
+
+      <PropositionsSection />
+    </>
   );
 }
